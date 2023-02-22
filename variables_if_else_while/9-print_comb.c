0905ccbb194eb8b7ba774ab0df4c65c53b4b8065
EmilioVS <6360@holbertonstudents.com>
2023-02-22 15:03:47 -0800

    Ejercicio 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Determine if the last digit of a random number is
+* greater than or less than 5, or is zero..
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+int number;
+for (number = '0'; number <='9'; number++)
+{
+putchar(number);
+if (number != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
