97fb9271268c3ea5a5c823ceb0af2b76744101a7
EmilioVS <6360@holbertonstudents.com>
2023-02-24 12:57:56 -0800

    Ejercicio 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_must_numbers - Prints the number 0 to 9
+* print_most_numbers - Prints the number 0 to 9
 * Description: Do not print 2 and 4
 * Return: The numbers0 to 9
 */
