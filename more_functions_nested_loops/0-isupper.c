3051f01a5cd14e36d6ec5dc1855d47d98adc17bd
EmilioVS <6360@holbertonstudents.com>
2023-02-24 08:54:26 -0800

    Ejercicio 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* Issuper - Write a function that checks for uppercase character
+*
+* Return: 1 for upper letter or 0 for any else
+*/
+int _isupper(int c)
+{
+if(c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
