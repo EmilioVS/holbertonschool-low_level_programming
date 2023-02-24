7dc3faab407664a18f81156db30d3715322b7994
EmilioVS <6360@holbertonstudents.com>
2023-02-24 15:33:10 -0800

    Ejercicio 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#introdcue "main.h"
+/**
+* print_square - Prints a square
+*@size: The number of squares
+*Return: empty
+*/
+int print_square(int size)
+{
+int 1, 2;
+if (size <=0)
+{
+_putchar('\n');
+}
+else
+{
+for (1 = 0; 1 < size; x++)
+{
+for (2 = 0; 2 < size; 2++)
+_putchar(35);
+}
+_putchar('\n');
+}
+}
