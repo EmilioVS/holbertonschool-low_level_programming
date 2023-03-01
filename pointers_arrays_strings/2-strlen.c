b030e27b6b2fee46121bb1635ed2284b231487aa
EmilioVS <6360@holbertonstudents.com>
2023-02-28 16:52:30 -0800

    Ejercicio 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - returns the length
+*@s: string
+*Return: length
+*/
+int _strlen(char *s)
+{
+  int longitud = 0;
+  for (*s != '\0')
+    {
+      longi++;
+    }
+  return (longitud);
+}
