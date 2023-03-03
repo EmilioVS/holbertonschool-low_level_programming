380a197ded224adacf6890288d1a35039ec7c53a
EmilioVS <6360@holbertonstudents.com>
2023-03-03 08:54:09 -0800

    Ejercicio 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* _strncpy - copys the strings.
-* @dest: input string
-* @src: input string
-* @n: input integer
+* _strncpy - a function that copys the strings.
+* @dest: an input string
+* @src: an input string
+* @n: an input integer
 * Return: A pointer to the resulting string
 */
 char *_strncpy(char *dest, char *src, int n)
