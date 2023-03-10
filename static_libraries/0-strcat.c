5d8fbd13b2bc38aea3ecee0d6d0420c90af85790
EmilioVS <6360@holbertonstudents.com>
2023-03-09 16:03:55 -0800

    Ejercicio 0

---
+++ static_libraries/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strcat - concatenates two strings.
+* @dest: input string
+* @src: input string
+* Return: 
+*/
+char *_strcat(char *dest, char *src)
+{
+char *temp = dest;
+while (*dest)
+dest++;
+while (*src)
+*dest++ = *src++;
+*dest = '\0';
+return (temp);
+}
---
+++ static_libraries/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - That checks if a character is a digit
+*@c: Is the number that haves to be checked
+*Return: 1 for a character that will be a digit 0 or for any else
+*/
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
