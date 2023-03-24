d04fa1c3e90f93e5e492aa0ed63bc7f64e642140
EmilioVS <6360@holbertonstudents.com>
2023-03-24 15:52:03 -0700

    Ejercicio 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-* main - Prints the result of simple operations.
-* @argc: The number of arguments supplied to the program.
-* @argv: An array of pointers to the arguments.
-* Return: Always 0.
-*/
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
+ */
 int main(int _attribute((unused_)) argc, char *argv[])
 {
 int num1, num2;
 char *op;
-if (argc != 4)
-{
+if (argc != 4)    {
 printf("Error\n");
 exit(98);
 }
