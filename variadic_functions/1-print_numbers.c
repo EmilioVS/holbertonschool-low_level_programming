d1b23c174d9f608fdf4b27d1600edebdebd05a13
EmilioVS <6360@holbertonstudents.com>
2023-03-24 13:23:58 -0700

    Ejercicio 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,51 +1,28 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 /**
-* print_all - A function prints anyting.
-* @format: A list of type of arguments passed to the function.
-* Return: Nothing
+* print_numbers - A function that print numbers followed by a new line.
+* @separator: An input string to be printed between numbers.
+* @n: number of parameters
+* @...: Other parameters
+* Return: The of all parameters
 */
-void print_all(const char * const format, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list ap;
-char *temp;
-int i = 0;
-va_start(ap, format);
-while (format == NULL)
+unsigned int i = 0;
+int nums;
+va_start(ap, n);
+for (; i < n; i++)
 {
+nums = va_arg(ap, int);
+printf("%d", nums);
+if (separator == NULL)
+continue;
+if (i < n - 1)
+printf("%s", separator);
+}
 printf("\n");
-return;
-}
-while (format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", (char) va_arg(ap, int));
-break;
-case 'i':
-printf("%d", va_arg(ap, int));
-break;
-case 'f':
-printf("%f", (float) va_arg(ap, double));
-break;
-case 's':
-temp = va_arg(ap, char*);
-if (temp != NULL)
-{
-printf("%s", temp);
-break;
-}
-printf("(nil)");
-break;
-}
-if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
-format[i] == 's') && format[(i + 1)] != '\0')
-printf(", ");
-i++;
-}
 va_end(ap);
-printf("\n");
 }
