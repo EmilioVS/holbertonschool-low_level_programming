1c8a1dfd9661b7ec340ba6e64b158cbf8818de24
EmilioVS <6360@holbertonstudents.com>
2023-04-07 12:03:11 -0700

    Ejercicio 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,64 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 /**
-* insert_dnodeint_at_index - A function that inserts a node at
-* position in a list.
-* @h: The double pointer to the head.
-* @idx: The index to insert new node at.
-* @n: The data to add to new node.
-* Return: A pointer to new element, or NULL on failure.
+* create_dnode - creates new node
+* @n: data of node
+* @prev: link to prev node
+* @next: link to next node
+* Return: pointer to new node
+*/
+dlistint_t *create_dnode(int n, dlistint_t *prev, dlistint_t *next)
+{
+dlistint_t *new;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->prev = prev;
+new->next = next;
+return (new);
+}
+/**
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: head of doubly-linked list
+* @idx: index for insertion of new node
+* @n: data for new node
+* Return: address of new node or NULL if error
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node = NULL, *temp = NULL;
-unsigned int i = 0;
-new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
+dlistint_t *curr = *h, *localPrev = NULL;
+unsigned int count = 0;
+if (!h)
 return (NULL);
-new_node->next = NULL;
-new_node->prev = NULL;
-new_node->n = n;
-if (!h || !(*h))
-*h = new_node;
+if (idx == 0)
+{
+if (!*h)
+*h = create_dnode(n, NULL, NULL);
 else
 {
-temp = *h;
-while (idx != i++ && temp->next)
-temp = temp->next;
-if (temp->next)
-new_node->prev = temp->prev;
-else
-new_node->prev = temp;
-if (idx == i)
-temp->next = new_node, new_node->prev = temp;
-else if (idx == i - 1)
-{
-if (temp->prev)
-temp->prev->next = new_node;
-else
-*h = new_node;
-temp->prev = new_node;
-new_node->next = temp;
+(*h)->prev = create_dnode(n, NULL, *h);
+*h = (*h)->prev;
 }
-else
+return (*h);
+}
+for (curr = *h; curr && (count < idx); curr = curr->next, count++)
 {
-free(new_node);
+localPrev = curr;
+}
+if ((count == idx) && (curr == NULL))
+{
+localPrev->next = create_dnode(n, localPrev, NULL);
+return (localPrev->next);
+}
+if ((count < idx) && (curr == NULL))
 return (NULL);
+if (localPrev != NULL)
+{
+localPrev->next = create_dnode(n, localPrev, curr);
+curr->prev = localPrev->next;
+return (localPrev->next);
 }
-}
-return (new_node);
+return (NULL);
 }
